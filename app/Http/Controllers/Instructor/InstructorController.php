722a8797d689015c2db820edabe7fd2f56d0e95a
poonam.k1 <poonam.k@infinitysoftsystems.com>
2024-10-01 13:11:15 +0530

    update instructor controller

--- app/Http/Controllers/Instructor/InstructorController.php
+++ app/Http/Controllers/Instructor/InstructorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Instructor;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use App\Services\InstructorService;
