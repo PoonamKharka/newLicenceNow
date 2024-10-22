9efc69b016fb413d8bbcbda34fa9ba2b787df281
poonam.k1 <poonam.k@infinitysoftsystems.com>
2024-10-22 15:53:32 +0530

    added middleware for cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin.gate' => \App\Http\Middleware\AdminGate::class,
-        'cors' => \App\Http\Middleware\CORS::class,
+        'cors' => \App\Http\Middleware\corsMiddleware::class,
     ];
 }
