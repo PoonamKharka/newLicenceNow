2927f1026b2acb3ad2c759866fae2792faba0b33
poonam.k1 <poonam.k@infinitysoftsystems.com>
2024-10-22 15:40:02 +0530

    added cors middleware

--- routes/api.php
+++ routes/api.php
@@ -22,34 +22,36 @@ Route::get('/documentation', function () {
     return view('vendor.l5-swagger.index');
 });
 
-# Authentication Route
-Route::post('instructor-register', [RegistrationController::class, 'instructorRegistrationRquest']);
-Route::post('register', [RegistrationController::class, 'registration']);
-Route::post('login', [RegistrationController::class, 'login']);
+Route::group(['middleware' => 'cors'], function() {
+   # Authentication Route
+    Route::post('instructor-register', [RegistrationController::class, 'instructorRegistrationRquest']);
+    Route::post('register', [RegistrationController::class, 'registration']);
+    Route::post('login', [RegistrationController::class, 'login']);
 
-# public website apis
-/** Article endpoints starts */
-Route::get('about', [ArticleController::class, 'getDataOfAboutUs']);
-Route::get('faqs', [ArticleController::class, 'getAllFaqs']);
-Route::get('articles-data', [ArticleController::class, 'getAllArticleModules']);
-Route::get('features', [ArticleController::class, 'getAllFeatures']);
-Route::get('article/privacy-policies',[ArticleController::class,'getAllPrivacyPolicies']);
-Route::get('nav-menu',[ArticleController::class,'getAllMenu']);
-/** Article endpoints ends */
+    # public website apis
+    /** Article endpoints starts */
+    Route::get('about', [ArticleController::class, 'getDataOfAboutUs']);
+    Route::get('faqs', [ArticleController::class, 'getAllFaqs']);
+    Route::get('articles-data', [ArticleController::class, 'getAllArticleModules']);
+    Route::get('features', [ArticleController::class, 'getAllFeatures']);
+    Route::get('article/privacy-policies',[ArticleController::class,'getAllPrivacyPolicies']);
+    Route::get('nav-menu',[ArticleController::class,'getAllMenu']);
+    /** Article endpoints ends */
 
-/** Home Page endpoints starts */
-Route::get('instructor-search',[SearchController::class,'getAvailableInstructors']);
-Route::get('instructors',[SearchController::class,'getAllInstructors']);
-Route::get('location-search',[SearchController::class,'getAvailableSuburbs']);
-Route::get('instructors/{id}',[SearchController::class,'getInstructorDetails']);
-/** Home Page endpoints ends */
+    /** Home Page endpoints starts */
+    Route::get('instructor-search',[SearchController::class,'getAvailableInstructors']);
+    Route::get('instructors',[SearchController::class,'getAllInstructors']);
+    Route::get('location-search',[SearchController::class,'getAvailableSuburbs']);
+    Route::get('instructors/{id}',[SearchController::class,'getInstructorDetails']);
+    /** Home Page endpoints ends */
 
-/** Booking steps endpoints starts */
-Route::get('prices',[BookingController::class,'getHoursList']);
-Route::get('test-package', [BookingController::class, 'getTestPackage']);
-Route::get('pricing-structure', [BookingController::class, 'pricingStructure']);
-/** Booking steps endpoints ends */
+    /** Booking steps endpoints starts */
+    Route::get('prices',[BookingController::class,'getHoursList']);
+    Route::get('test-package', [BookingController::class, 'getTestPackage']);
+    Route::get('pricing-structure', [BookingController::class, 'pricingStructure']);
+    /** Booking steps endpoints ends */
 
-/** Common APIs starts */
-Route::get('states', [CommanAPIController::class, 'getStates']);
-/** Common APIs ends */
+    /** Common APIs starts */
+    Route::get('states', [CommanAPIController::class, 'getStates']);
+    /** Common APIs ends */ 
+});
